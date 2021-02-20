3387e3f794ab5a3c44b5bf801509b6ca170a731e
Ersikan <julien.philippon@epitech.eu>
2021-02-20 20:02:11 +0100

    Remove all the old API

--- source/main.cpp
+++ source/main.cpp
@@ -1,15 +1,7 @@
 #include <iostream>
-#include "Library.hpp"
-#include "Pipeline.hpp"
-#include "Zia.hpp"
 
-int main(void) {
-    try {
-        Library<zia::protocol::INewClient> mod("lib/libzia_test_module.so");
-
-        mod->onNewClient("127.0.0.1", 2908);
-    } catch (const LibraryErr &le) {
-        std::cerr << le.getMessage() << std::endl;
-    }
+int main(void)
+{
+    std::cout << "Hello world!" << std::endl;
     return 0;
 }
