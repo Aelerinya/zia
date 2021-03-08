76634288dfeef64a20faeec137e4e73883200d0c
Ersikan <julien.philippon@epitech.eu>
2021-03-08 16:58:32 +0100

    refactor(test_module): Listen to start event

--- module/test/test_module.cpp
+++ module/test/test_module.cpp
@@ -1,4 +1,5 @@
 #include "test_module.hpp"
+#include "api/internal/onstart.hpp"
 #include "api/mediator.hpp"
 #include <iostream>
 
@@ -23,14 +24,19 @@ void test::Module::configureModule([[maybe_unused]] const YAML::Node &config)
 extern "C" std::unique_ptr<api::IModule>
 load_module([[maybe_unused]] api::IZiaInitializer &initializer)
 {
+    initializer.registerConsumer(
+        api::event_descriptor<api::OnStartEvent>,
+        [](api::IZiaMediator &mediator, std::unique_ptr<api::IEvent>) {
+            std::clog << "Start event called" << std::endl;
+            mediator.emit(std::make_unique<test::Event>());
+        });
     initializer.registerListener(api::event_descriptor<test::Event>,
                                  [](const api::IZiaMediator &, const api::IEvent &) {
                                      std::clog << "Listener called" << std::endl;
                                  });
     initializer.registerConsumer(api::event_descriptor<test::Event>,
-                                 [](api::IZiaMediator &mediator, std::unique_ptr<api::IEvent>) {
+                                 [](api::IZiaMediator &, std::unique_ptr<api::IEvent>) {
                                      std::clog << "Consumer called" << std::endl;
-                                     mediator.emit(std::make_unique<test::Event>());
                                  });
     return std::make_unique<test::Module>();
 }
--- tests/test_module_proxy.cpp
+++ tests/test_module_proxy.cpp
@@ -1,24 +1,14 @@
-#include "ModuleProxy.hpp"
-#include "TestInitializer.hpp"
-#include "TestMediator.hpp"
-#include "module/test/test_module.hpp"
 #include <boost/test/unit_test.hpp>
 #include <yaml-cpp/yaml.h>
 
+#include "ModuleProxy.hpp"
+#include "TestInitializer.hpp"
+#include "TestMediator.hpp"
+#include "api/internal/onstart.hpp"
+#include "module/test/test_module.hpp"
+#include "TestDisplay.hpp"
+
 using namespace zia;
-
-class TestEvent : public zia::api::IEvent
-{
-    const api::EventDescriptor &getDescriptor() const override;
-};
-
-template <>
-constexpr zia::api::EventDescriptor zia::api::event_descriptor<TestEvent> = {"TestEvent"};
-
-const api::EventDescriptor &TestEvent::getDescriptor() const
-{
-    return zia::api::event_descriptor<TestEvent>;
-}
 
 BOOST_AUTO_TEST_CASE(test_ModuleProxy)
 {
@@ -27,18 +17,28 @@ BOOST_AUTO_TEST_CASE(test_ModuleProxy)
     test::Mediator mediator;
     zia::ModuleProxy proxy("lib/testmodule", initializer);
 
-    std::clog << initializer.consumers.at(0).first.get().name << std::endl;
     BOOST_TEST(proxy.getName() == test::Module::module_name);
-    std::clog << "getting consumer" << std::endl;
-    BOOST_TEST(initializer.consumers.at(0).first.get().name ==
-               api::event_descriptor<test::Event>.name);
-    initializer.consumers.at(0).second(mediator, std::make_unique<TestEvent>());
-    BOOST_TEST(mediator.events.at(0)->getDescriptor().name ==
-               api::event_descriptor<test::Event>.name);
-    initializer.listeners.at(0).second(mediator, TestEvent());
-    std::clog << "getting listener" << std::endl;
-    BOOST_TEST(initializer.listeners.at(0).first.get().name ==
-               api::event_descriptor<test::Event>.name);
+
+    std::clog << "getting start consumer" << std::endl;
+    BOOST_TEST(initializer.consumers.at(0).first.get() ==
+               api::event_descriptor<zia::api::OnStartEvent>);
+    initializer.consumers.at(0).second(mediator,
+                                       std::make_unique<zia::api::OnStartEvent>());
+    BOOST_TEST(mediator.events.at(0)->getDescriptor() ==
+               api::event_descriptor<test::Event>);
+
+    // Extracting event from the mediator
+    std::unique_ptr<zia::api::IEvent> event;
+    mediator.events.at(0).swap(event);
+    mediator.events.clear();
+
+    std::clog << "getting test listener" << std::endl;
+    initializer.listeners.at(0).second(mediator, *event.get());
+    BOOST_TEST(initializer.listeners.at(0).first.get() ==
+               api::event_descriptor<test::Event>);
+
+    std::clog << "getting test consumer" << std::endl;
+
     std::clog << "end" << std::endl;
     initializer.consumers.clear();
     initializer.listeners.clear();
