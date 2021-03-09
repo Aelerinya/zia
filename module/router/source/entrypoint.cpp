18ceff67f182a76f87a57e396f7a04781f4a6a5d
Ersikan <julien.philippon@epitech.eu>
2021-03-09 14:47:54 +0100

    feat(router): Improve logging and errors

--- module/router/source/entrypoint.cpp
+++ module/router/source/entrypoint.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include "api/http/request.hpp"
 #include "api/initializer.hpp"
 #include "api/mediator.hpp"
@@ -11,13 +13,21 @@ load_module(zia::api::IZiaInitializer &initializer)
 
     initializer.registerConsumer(
         zia::api::event_descriptor<zia::api::http::NewHTTPRequest>,
-        [&router = *router](zia::api::IZiaMediator &mediator, std::unique_ptr<zia::api::IEvent> e) {
+        [&router = *router](zia::api::IZiaMediator &mediator,
+                            std::unique_ptr<zia::api::IEvent> e) {
             auto event = zia::dynamic_unique_ptr_cast<zia::api::http::NewHTTPRequest>(
                 std::move(e));
+            std::clog << "Routing request to " << event->getRequest().route << std::endl;
+
             auto route_result = router.routeRequest(event->getRequest().route);
 
             if (!route_result.has_value()) {
-                zia::api::http::HTTPResponse not_found{404, "Not found", {}, ""};
+                zia::api::http::HTTPResponse not_found{
+                    404,
+                    "Not found",
+                    {},
+                    "Router did not find any handler for route " +
+                        event->getRequest().route};
                 mediator.emit(event->createResponse(not_found));
             } else {
                 std::unique_ptr<zia::api::IEvent> route_request =
