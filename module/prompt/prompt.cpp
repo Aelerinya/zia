14acc85812a91817ba4a75987945d2ba021804a9
Maëlys Bras de fer <bastien.bras-de-fer@epitech.eu>
2021-03-10 13:39:38 +0100

    Prompt module's prompt might be working

--- module/prompt/prompt.cpp
+++ module/prompt/prompt.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 #include "prompt.hpp"
+#include "ConfigLoader.hpp"
+#include "api/mediator.hpp"
 
 using namespace zia::prompt;
 using namespace zia;
@@ -15,8 +17,25 @@ void Module::configureModule([[maybe_unused]] const YAML::Node &config)
 {
 }
 
-void Module::start(api::IZiaMediator &)
+void Module::start(api::IZiaMediator &mediator)
 {
     std::clog << "Launching prompt..." << std::endl;
- // TODO: laucnh prompt
+
+    m_prompt_thread = std::jthread {
+        [&](std::stop_token stopped) {
+            while (!stopped.stop_requested()) {
+
+                if (std::cin.rdbuf()->in_avail()) {
+                    std::string line;
+                    std::getline(std::cin, line);
+
+                    if (line == "reload")
+                        mediator.emit(std::make_unique<ReloadConfig>());
+                    else
+                        std::cerr << "Unknown command '" << line << "'\n";
+                } else
+                    std::this_thread::yield();
+            }
+        }
+    };
 }
